6f299434324c8e67edf44e7cdc19ce9a34ef16d5
belial4ik <belyal4ik@gmail.com>
2023-02-07 13:39:24 +0000

    add date on todo and fix validation

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import Header from './components/Header/Header';
 import TodoForm from './components/TodoForm/TodoForm';
 import TodoList from './components/TodoList/TodoList';
 
-const initialState = [
-  { id: 1, title: 'Learn English', isCompleted: false },
-  { id: 2, title: 'Have a lunch', isCompleted: false }
-]
-
 function App() {
   const [todoText, setTodoText] = useState('')
-  const [todos, setTodos] = useState(initialState)
+  const [todos, setTodos] = useState([])
 
   //Submit form
   const submitForm = event => {
     event.preventDefault()
-    const newTodo = { id: Math.floor(Math.random() * 10000), title: todoText, isCompleted: false }
+    const currentTime = `
+    ${new Date().getDate().toString()}.0${+ new Date().getDay().toString() + 1}.${new Date().getFullYear().toString().substring(2)}
+    ${new Date().getHours().toString()}:${new Date().getMinutes().toString() < 10 ? 0 + new Date().getMinutes().toString() : new Date().getMinutes().toString()}:${new Date().getSeconds().toString() < 10 ? 0 + new Date().getSeconds().toString() : new Date().getSeconds().toString()}
+    `
+    const newTodo = { id: Math.floor(Math.random() * 10000), title: todoText, time: currentTime, isCompleted: false }
     const newTodos = [...todos, newTodo]
 
-    //Validation
-    if (todoText.length > 0 && todoText.length < 21) {
+    if (todoText.length > 0 && todoText.length < 21 && todoText.trim() !== '') {
       setTodos(newTodos)
     } else {
-      alert('от 1 до 20 символов')
+      alert('can`t be empty and contain from 1 - 20 symbols')
     }
     setTodoText('')
   }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -5,12 +5,15 @@ const TodoList = props => {
   return (
     <>
       {
-        props.todos.map(({ id, title, isComplete }) => (
-          <div key={id} className={`${classes.todoList} container`}>
-            <h3
-              onClick={() => props.onComplete(id)}
-            >{title}</h3>
-            <span onClick={() => props.onDelete(id)} ><AiFillDelete /></span>
+        props.todos.map(({ id, title, time, isComplete }) => (
+          <div className={`${classes.todoList} container`}>
+            <div key={id}>
+              <h3 onClick={() => props.onComplete(id)}>{title}</h3>
+            </div>
+            <div>
+              <span className={classes.timeOfTodo}>{time}</span>
+              <span className={classes.deleteBtn} onClick={() => props.onDelete(id)}><AiFillDelete /></span>
+            </div>
           </div>
         ))
       }
